b44bf3abea5cc092a33b5c5db3dae2ba4e3d9934
s27542 <s27542@pjwstk.edu.pl>
2024-04-12 14:50:15 +0200

    Added Project

---
+++ projekt2/projekt2/Controllers/AnimalController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace projekt2.Controllers;
+[ApiController]
+[Route("aniapi/animals")]
+public class AnimalController : Controller
+{
+ private readonly IConfiguration _configuration;
+ public AnimalController(IConfiguration configuration)
+ {
+  _configuration = configuration;
+ }
+ 
+ [HttpGet]
+ public IActionResult GetAnimals()
+ {
+  //Otwieramy polaczenie do bazy
+  SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+  connection.Open();
+  
+  return Ok();
+ }
+}
